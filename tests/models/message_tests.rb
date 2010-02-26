4c36cb66d3bfe3b4ad4c10e8b2be01a39648ff61
Raving Genius <rg+code@ravinggenius.com>
2010-02-26 00:46:01 -0500

    Added some more test for timezones

--- tests/models/message_tests.rb
+++ tests/models/message_tests.rb
@@ -1,13 +1,14 @@
 require 'shoulda'
 require 'json'
+require 'tzinfo'
 
 require File.join(File.dirname(__FILE__), '/../../models/message')
 
 class MessageTests < Test::Unit::TestCase
-  context 'A new Message' do
+  context 'A new Message for UTC' do
     setup do
-      @now = Time.local 2010
-      @m = Message.new @now
+      @time = Time.utc 2010
+      @m = Message.new :time => @time
     end
 
     should 'get the basics right' do
@@ -21,6 +22,46 @@ class MessageTests < Test::Unit::TestCase
     end
 
     should 'correctly convert to JSON' do
+      expected = JSON.parse '{
+  "answer": "No",
+  "comment": "What a drag.",
+  "next_event": {
+    "hours": 17,
+    "minutes": 0,
+    "seconds": 0
+  }
+}'
+      assert_equal expected, JSON.parse(@m.to_json)
+    end
+  end
+
+  context 'A new Message for local time' do
+    setup do
+      @time = Time.utc 2010
+      @m = Message.new :time => @time, :zone => 'America/New_York'
+    end
+
+    should 'correctly convert to JSON' do
+      expected = JSON.parse '{
+  "answer": "No",
+  "comment": "What a drag.",
+  "next_event": {
+    "hours": 22,
+    "minutes": 0,
+    "seconds": 0
+  }
+}'
+      assert_equal expected, JSON.parse(@m.to_json)
+    end
+  end
+
+  context 'A new Message with a invalid timezone given' do
+    setup do
+      @time = Time.utc 2010
+      @m = Message.new :time => @time, :zone => 'Solar_System/Mars'
+    end
+
+    should 'default to Etc/UTC and correctly convert to JSON' do
       expected = JSON.parse '{
   "answer": "No",
   "comment": "What a drag.",
