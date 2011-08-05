177781855302e4ba9735e1a0ae723abfcc73f0c8
Raving Genius <rg+code@ravinggenius.com>
2011-08-05 00:18:17 -0400

    Model cleanup

--- models/message.rb
+++ models/message.rb
@@ -2,23 +2,28 @@ require 'active_support/core_ext'
 require 'tzinfo'
 
 class Message
-  def initialize options = {}
-    @right_now = begin
-      TZInfo::Timezone.get options[:zone]
-    rescue TZInfo::InvalidTimezoneIdentifier
-      TZInfo::Timezone.get 'Etc/UTC'
-    end.utc_to_local(options[:time] || Time.new.utc)
+  def initialize(options = {})
+    @right_now = lambda do
+      begin
+        TZInfo::Timezone.get options[:zone]
+      rescue TZInfo::InvalidTimezoneIdentifier
+        TZInfo::Timezone.get 'Etc/UTC'
+      end.utc_to_local(options[:time] || Time.new.utc)
+    end.call
 
-    @next_event = @right_now.monday + (is_weekend? ? 1.week + 8.hours : 4.days + 17.hours)
-    @next_event = (@next_event - 1.week) if (@right_now.wday == 1) && (@right_now.hour < 8)
+    @next_event = lambda do
+      reply = @right_now.monday + (is_weekend? ? 1.week + 8.hours : 4.days + 17.hours)
+      (@right_now.monday? && (@right_now.hour < 8)) ? (reply - 1.week) : reply
+    end.call
   end
 
   def is_weekend?
-    @is_weekend ||= case @right_now.wday
-      when 0, 6 then true                  # sunday, saturday
-      when    1 then @right_now.hour < 8   # monday
-      when    5 then @right_now.hour >= 17 # friday
-      else           false
+    case
+      when @right_now.sunday?   then true
+      when @right_now.monday?   then @right_now.hour < 8
+      when @right_now.friday?   then @right_now.hour >= 17
+      when @right_now.saturday? then true
+      else                           false
     end
   end
 
@@ -36,20 +41,26 @@ class Message
     offset = (@next_event - @right_now).to_f
 
     offset = offset / 60 / 60
-    reply[:hour] = offset.floor
+    reply[:hours] = offset.floor
 
     offset = (offset - offset.floor) * 60
-    reply[:minute] = offset.floor
+    reply[:minutes] = offset.floor
 
     offset = (offset - offset.floor) * 60
-    reply[:second] = offset.round
+    reply[:seconds] = offset.round
 
     reply
   end
 
+  def as_json
+    {
+      :answer => answer,
+      :comment => comment,
+      :next_event => countdown
+    }
+  end
+
   def to_json
-    cd = countdown
-    h, m, s = cd[:hour], cd[:minute], cd[:second]
-    "{\"answer\":\"#{answer}\",\"comment\":\"#{comment}\",\"next_event\":{\"hours\":#{h},\"minutes\":#{m},\"seconds\":#{s}}}"
+    as_json.to_json
   end
 end
