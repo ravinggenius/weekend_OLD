e101840e0eb98696f3c5964c9b934c697c7f2d89
Raving Genius <rg+code@ravinggenius.com>
2012-06-24 23:14:09 -0400

    Refactor Message test

--- spec/weekend/message_spec.rb
+++ spec/weekend/message_spec.rb
@@ -3,95 +3,58 @@ require_relative '../spec_helper'
 require 'weekend/message'
 
 describe Message do
-  subject { Message.new }
+  subject { Message.new :time => time }
 
-  it 'counts down the longest on Monday morning at 8:00'
+  context 'at the very start of the timer' do
+    it 'counts down the longest on Monday morning at 8:00'
+  end
 
-  describe 'an invalid timezone' do
+  context 'an invalid timezone' do
     let(:time) { Time.utc 2010 }
     subject { Message.new :time => time, :zone => 'Solar_System/Mars' }
 
-    it 'is not the weekend' do
-      expect(subject.is_weekend?).to be_false
-      expect(subject.answer).to eq('No')
-    end
-
-    it 'countdown must be accurate' do
-      expect(subject.countdown).to eq(:hours => 17, :minutes => 0, :seconds => 0)
-    end
+    it { should_not be_is_weekend }
+    its(:countdown) { should == { :hours => 17, :minutes => 00, :seconds => 00 } }
   end
 
-  describe 'Monday morning' do
-    let(:time) { Time.utc 2010, 3, 1, 7, 15, 0 }
-    subject { Message.new :time => time }
+  context 'Monday morning' do
+    let(:time) { Time.utc 2010, 03, 01, 07, 15, 00 }
 
-    it 'is the weekend' do
-      expect(subject.is_weekend?).to be_true
-      expect(subject.answer).to eq('Yes')
-    end
-
-    it 'countdown must be accurate' do
-      expect(subject.countdown).to eq(:hours => 0, :minutes => 45, :seconds => 0)
-    end
+    it { should be_is_weekend }
+    its(:countdown) { should == { :hours => 00, :minutes => 45, :seconds => 00 } }
   end
 
-  describe 'UTC' do
-    describe 'A new weekend Message for UTC' do
-      let(:time) { Time.utc 2010, 10, 10, 10, 10, 10 }
-      subject { Message.new :time => time }
+  context 'on the weekend (UTC)' do
+    let(:time) { Time.utc 2010, 10, 10, 10, 10, 10 }
 
-      it 'is the weekend' do
-        expect(subject.is_weekend?).to be_true
-        expect(subject.answer).to eq('Yes')
-      end
-
-      it 'countdown must be accurate' do
-        expect(subject.countdown).to eq(:hours => 21, :minutes => 49, :seconds => 50)
-      end
-    end
-
-    describe 'A new weekday Message for UTC' do
-      let(:time) { Time.utc 2011, 11, 11, 11, 11, 11 }
-      subject { Message.new :time => time }
-
-      it 'is not the weekend' do
-        expect(subject.is_weekend?).to be_false
-        expect(subject.answer).to eq('No')
-      end
-
-      it 'countdown must be accurate' do
-        expect(subject.countdown).to eq(:hours => 5, :minutes => 48, :seconds => 49)
-      end
-    end
+    it { should be_is_weekend }
+    its(:answer) { should == 'Yes' }
+    its(:countdown) { should == { :hours => 21, :minutes => 49, :seconds => 50 } }
   end
 
-  describe 'Local' do
-    describe 'A new weekday Message' do
-      let(:time) { Time.utc 2010, 01, 05, 13, 45, 37 }
-      subject { Message.new :time => time, :zone => 'America/New_York' }
+  context 'during the week (UTC)' do
+    let(:time) { Time.utc 2011, 11, 11, 11, 11, 11 }
 
-      it 'is not the weekend' do
-        expect(subject.is_weekend?).to be_false
-        expect(subject.answer).to eq('No')
-      end
+    it { should_not be_is_weekend }
+    its(:answer) { should == 'No' }
+    its(:countdown) { should == { :hours => 5, :minutes => 48, :seconds => 49 } }
+  end
 
-      it 'countdown must be accurate' do
-        expect(subject.countdown).to eq(:hours => 80, :minutes => 14, :seconds => 23)
-      end
-    end
+  context 'on the weekend (local)' do
+    let(:time) { Time.utc 2011, 01, 01, 16, 35, 12 }
+    subject { Message.new :time => time, :zone => 'America/New_York' }
 
-    describe 'A new weekend Message' do
-      let(:time) { Time.utc 2011, 01, 01, 16, 35, 12 }
-      subject { Message.new :time => time, :zone => 'America/New_York' }
+    it { should be_is_weekend }
+    its(:answer) { should == 'Yes' }
+    its(:countdown) { should == { :hours => 44, :minutes => 24, :seconds => 48 } }
+  end
 
-      it 'is the weekend' do
-        expect(subject.is_weekend?).to be_true
-        expect(subject.answer).to eq('Yes')
-      end
+  context 'during the week (local)' do
+    let(:time) { Time.utc 2010, 01, 05, 13, 45, 37 }
+    subject { Message.new :time => time, :zone => 'America/New_York' }
 
-      it 'countdown must be accurate' do
-        expect(subject.countdown).to eq(:hours => 44, :minutes => 24, :seconds => 48)
-      end
-    end
+    it { should_not be_is_weekend }
+    its(:answer) { should == 'No' }
+    its(:countdown) { should == { :hours => 80, :minutes => 14, :seconds => 23 } }
   end
 end
